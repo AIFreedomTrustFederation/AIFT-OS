ee17cbd810fd9e639a30091f99eff309e6d5f950
AIFT Federation <aifreedomtrust@gmail.com>
2026-08-06 11:10:37 -0700

    feat: compose geometry runtime into federation games

--- internal/uxihttp/game.go
+++ internal/uxihttp/game.go
@@ -1,9 +1,27 @@
 package uxihttp
 
-import _ "embed"
+import (
+	_ "embed"
+	"strings"
+)
 
 //go:embed tree-game.html
-var treeGameHTML string
+var treeGameBaseHTML string
 
 //go:embed world-game.html
-var worldGameHTML string
+var worldGameBaseHTML string
+
+//go:embed geometry-runtime.js
+var geometryRuntimeJS string
+
+var treeGameHTML = composeGameHTML(treeGameBaseHTML)
+var worldGameHTML = composeGameHTML(worldGameBaseHTML)
+
+func composeGameHTML(base string) string {
+	const marker = "</body>"
+	if !strings.Contains(base, marker) {
+		panic("Federation game composition marker not found")
+	}
+	script := "  <script>\n" + geometryRuntimeJS + "\n  </script>\n"
+	return strings.Replace(base, marker, script+marker, 1)
+}
